6d791995b3f4166ac9256e8e7e546a291321c3ab
Kate <noskova2111@mail.ru>
2022-11-10 19:36:41 +0300

    10.11

--- src/js/script.js
+++ src/js/script.js
@@ -44,7 +44,7 @@ console.log(gerRandom(number1, number2));  */
 
 // 1 Что выведет console.log / Ответ 9
 //разобрать после циклов 
-for (var i=0; i<9; i+=2 ) {
+/* for (var i=0; i<9; i+=2 ) {
     if(++i == 5) {
         break;
     } else {
@@ -52,21 +52,21 @@ for (var i=0; i<9; i+=2 ) {
     }
 }
 console.log(i++); 
-
+ */
 //Объяснение:
 //условие if никогда не выполнится, поэтому i дойдёт до 9 и цикл прекратится. Далее, используется оператор пост-инкремента, который, присвоит в i 10, а потом вернёт её старое значение 9
 
 
 // 2 Как вывести в консоль название ОС? [Node.js]
 // Allocating os module
-const os = require('os');
+/* const os = require('os');
   
 // Printing os.type() value
 console.log(os.type());
-
+ */
 
 // 3 Что выведет консоль?  /  Ford
-const carDetails = {
+/* const carDetails = {
     name: 'Ford',
     year: 2005,
     getName() {
@@ -75,9 +75,9 @@ const carDetails = {
     isRegistred: true
 
 };
-console.log(carDetails.getName());
+console.log(carDetails.getName()); */
 
-
+/* 
 // 4 Что выведет консоль?  / "string"
 //ответ typeof typeof null - string, typeof null - object
 console.log( typeof typeof null); 
@@ -88,11 +88,11 @@ const hi = ["buy", "sell"];
 const string = {book: "red"};
 
 console.log( Object.prototype.toString( hi ));
-console.log( typeof( string )); 
+console.log( typeof( string ));  */
 
 
 // 6 Какой будет результат?
-var a = 44;
+/* var a = 44;
 (function() {
     var b = 44;
     a = +'55';
@@ -101,9 +101,9 @@ var a = 44;
         var c = 11;
         alert((a + b) / c);
     })();
-})();
+})(); */
  
-// 7 Что покажет  alert?  / Ответ : 1
+/* // 7 Что покажет  alert?  / Ответ : 1
 var a = -1;
 var b = 0.25;
 //alert( a == ~b^0);
@@ -146,24 +146,24 @@ var myObj = {
 };
 console.log(myObj.hasOwnProperty("name")); //true
 console.log(myObj.hasOwnProperty("age")); //false
-
+ */
 //Метод hasOwnProperty() возвращает логическое значение, указывающее, содержит ли объект указанное свойство.
 
 
 //  12 Что выведет консоль?
-const obj = {};
+/* const obj = {};
 Object.defineProperty(obj, 'bestsite', {value: 'proghub'});
 
 console.log(obj.bestsite); // это вернет proghub
 delete obj.bestsite;
-console.log(obj.bestsite); // это вернет TypeError
+console.log(obj.bestsite); // это вернет TypeError */
 
 //Метод Object.defineProperty() определяет новое или изменяет существующее свойство непосредственно на объекте, возвращая этот объект.
 
 
 //  13 Какими будут значения переменных a и b после выполнения данного фрагмента кода? 
 //а-'Hello'; b-underfined
-function Test(param) {
+/* function Test(param) {
     this.var1 = param;
     var var2 = 'World';
 }
@@ -174,85 +174,18 @@ var b = test.var2;
 
 console.log(a);
 console.log(b);
+ */
 
 
+//  14 Найти вес всех вещей, цена которых более 80 и количество менее 7. Найти сумму найденый вещей
+const food = [
+    {name: 'Паста "Болоньезе"', weight: 350, price: 68, quantity: 10},
+    {name: 'Спагетти с овощами', weight: 350, price: 56, quantity: 8},
+    {name: 'Пене с кур филе', weight: 400, price: 68, quantity: 16},
+    {name: 'Пицца с ананасами', weight: 675, price: 139, quantity: 30},
+    {name: 'Пицца четыре сыра', weight: 1600, price: 339, quantity: 8},
+    {name: 'Пицца Итальянская', weight: 740, price: 159, quantity: 1},
+    {name: 'Пицца с семгой', weight: 230, price: 77, quantity: 4},
+    {name: 'Салат цезарь', weight: 230, price: 69, quantity: 5},
+];
 
-
-
-
-
-
-
-
-//1.    Выведите столбец чисел от 1 до 100.
-let q = 1;
-
-while (q <= 100) {
-    document.write(q + "<br>");
-    q++;
-}  
-
-for(let w = 0; w <= 100; w++) {
-    document.write(w + '<br>');
-}
-
-//2.     Выведите столбец чисел от 11 до 33.
-
-let r = 11;
-while (r < 34) {
-    document.write(r + "<br>");
-    r++;
-} 
-
-for(let i = 11; i <=33; i++) {
-    document.write(i + ',' + ' ');
-}
- 
-//2.1   Выведите d строку через запятую числа от 25 до 1.
-let p = 25;
-while(p >= 1) {
-    document.write(p + "," + " ");
-    p--;
-}   
-
-//3.    Выведите столбец четных чисел в промежутке от 0 до 100.
-let o = 0;
-while(o <= 100) {
-    document.write(o + "<br>");
-    o+=2;
-} 
-
-let t = 1;
-while(t <= 100) {
-    if(t % 2 == 0) {
-        document.write(t + '<br>');
-    }
-    t++;
-} 
-
-
-//4.  С помощью цикла найдите сумму чисел от 1 до 100.
-let sum = 0;
-for(i = 0; i <=100; i++) {
-    sum += i;
-}
-
-console.log(sum);
-
-//5.    Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for выведите все эти элементы на экран.
-let y = [1, 2, 3, 4, 5];
-for(let i = 1; i <= 5; i++) {
-    document.write(i);
-}
-
-
-//6.    Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for найдите сумму элементов этого массива. Запишите ее в переменную result.
-let u = [1, 2, 3, 4, 5];
-let result = 0;
-for(let i = 0; i < a.length; i++) {
-    result += c[i];   
-}
-console.log(result);
-
-
- 
--- src/js/tempCodeRunnerFile.js
+++ src/js/tempCodeRunnerFile.js
@@ -1,5 +1,3 @@
-const hi = ["buy", "spend"];
-const string = {kgl: "jdkf"};
-
-console.log( Object.prototype.toString( hi ));
-console.log( typeof( string )); 
+let x = 7;
+console.log(x++);
+console.log(x);
