48be55affdec2b7ce8a90f7f6c2cb9414b91a136
Kate2111 <noskova2111@mail.ru>
2023-01-27 21:47:42 +0400

    27.01

--- src/js/12_timer.js
+++ src/js/12_timer.js
@@ -100,10 +100,109 @@ function task6() {
 }
 task6();
 
-//7. 
+//7. Сделайте так, чтобы по нажатию на кнопку, значение этой кнопки каждую секунду увеличивалось на 1. Используйте передачу контекста(this) разными способами.
 function task7() {
-   
+    let elem = document.querySelector('.task12_7 #elem');
+    
+    //через переменную self
+        /* elem.addEventListener('click', function() {
+            let self = this;
+            setInterval(function() {
+                self.value = Number(self.value) + 1;
+            }, 1000);
+        }); */
+
+    //через стрелочную функцию
+        /* elem.addEventListener('click', function() {
+            setInterval(() => {
+                this.value = Number(this.value) + 1;
+            }, 1000);
+        }); */
+
+    //через передачу параметром
+        elem.addEventListener('click', function() {
+            setInterval(function(self) {
+                self.value = Number(self.value) + 1;
+            }, 1000, this);
+        }); 
 }
 task7();
 
+//8. Дан инпут с числом. Сделайте так, чтобы каждую секунду в нем появлялся квадрат того числа, которое в нем записано.
+function task8() {
+    const elem = document.querySelector('.task12_8 #elem');
+    function start () {
+        let timerId = setInterval(() => {
+            elem.value = Math.pow(+elem.value, 2);
+            if(elem.value == 65536) {
+                clearInterval(timerId);
+            }
+        }, 1000);
+    }
+    start();
 
+    elem.addEventListener('click', () => {
+        elem.value = 2;
+        start();
+    });
+
+}
+task8();
+
+//9. Дан инпут, кнопка и абзац. В инпут вводится какое-то число. По нажатию на кнопку запишите введенное число в текст абзаца и запустите обратный отсчет в абзаце: пусть каждую секунду число в абзаце уменьшается на единицу, пока не дойдет до нуля.
+function task9() {
+    const input = document.querySelector('.task12_9 input');
+    const div = document.querySelector('.task12_9 div');
+    const btn = document.querySelector('.task12_9 button');
+
+    btn.addEventListener('click', function() {
+        let i = +input.value;
+        let timerId = setInterval(() => {
+            console.log(i, typeof(i));
+            div.textContent = --i;
+            if(div.textContent <= 0){
+                clearInterval(timerId);
+            } 
+        }, 1000);
+    });
+}
+task9();
+
+//10. Дан абзац. Сделайте так, чтобы каждую секунду он менял свой цвет с красного на зеленый и наоборот.
+function task10() {
+    const div = document.querySelector('.task12_10 div');
+    setInterval(() => {
+        if(div.style.color == 'green') {
+            div.style.color = 'red';
+        } else {
+            div.style.color = 'green';
+        }
+    }, 1000);
+}
+task10();
+
+//11. Если выводить на экран каждую секунду текущий момент времени, то можно сделать тикающие часы. Реализуйте такие же часики
+function task11() {
+    const div = document.querySelector('.task12_11 div');
+    setInterval(() => {
+        let nowDay = new Date();
+        div.textContent = nowDay.getHours() + ':' + nowDay.getMinutes() + ':' + nowDay.getSeconds();
+        if(nowDay.getMinutes() <= 9) {
+            div.textContent = nowDay.getHours() + ':' + 0 + nowDay.getMinutes() + ':' + nowDay.getSeconds();
+        }
+        if(nowDay.getSeconds() <= 9) {
+            div.textContent = nowDay.getHours() + ':' + nowDay.getMinutes() + ':' + nowDay.getSeconds() + 0;
+        }
+    }, 1000);
+}
+task11();
+
+//12. Дан абзац. Напишите код, который выведет сообщение в этот абзац через 10 секунд после загрузки страницы.
+function task12() {
+    const div = document.querySelector('.task12_12 div');
+
+    setTimeout(function() {
+        div.textContent = 'Прошло 10 секунд';
+    }, 10000);
+}
+task12();
